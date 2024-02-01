e43dd890f15193ba6ba17e26684792b210628e37
Christopher Parsons <786967+cwparsons@users.noreply.github.com>
2024-02-01 09:30:05 -0800

    Initial Yeoman generation

---
+++ src/extensions/redirectExtension/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IRedirectExtensionApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'RedirectExtensionApplicationCustomizerStrings' {
+  const strings: IRedirectExtensionApplicationCustomizerStrings;
+  export = strings;
+}
